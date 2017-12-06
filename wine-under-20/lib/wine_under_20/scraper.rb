1947a5f4d3f0f2435bdf8c096e1c34406f577637
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-12-06 02:57:04 +0000

    automatically backed up by learn

--- wine-under-20/lib/wine_under_20/scraper.rb
+++ wine-under-20/lib/wine_under_20/scraper.rb
@@ -1,18 +1,18 @@
 class WineUnder20::Scraper
 
   def self.scrape_wines
-    wines = []
-    @doc = Nokogiri::HTML(open("https://www.binnys.com/wine//1/72#"))
-    @doc.search("div.row.result").each do |wine_div|
-      wine = WineUnder20::Wine.new
+    #wines = []
+    doc = Nokogiri::HTML(open("https://www.binnys.com/wine//1/72#"))
+    doc.search("div.row.result").each do |wine_div|
+      wine = WineUnder20::Wine.new(name, price, url, description)
       wine.url = wine_div.search("h3.title").attr("href")
       wine.name = wine_div.search("h3.title").text.strip
       wine.price = wine_div.search("div.prodPrice.unitPrice.salePrice").text.sub /\s*\(.+\)$/, ''.strip
       wine.description = wine_div.search("div.shortReview").text.sub("Read more", "").strip
 
-      wines << wine
+      #wines << wine
     end
-    wines
+    #wines
 
   end
 end
